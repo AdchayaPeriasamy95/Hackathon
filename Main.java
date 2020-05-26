dad5f8e84dcf024f521094eb3a66072b89afb422
AdchayaPeriasamy95 <65910477+AdchayaPeriasamy95@users.noreply.github.com>
2020-05-26 14:18:15 +0530

    Update and rename Adchu.java to Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.math.RoundingMode;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Adchu {
+public class Main {
 
 	public static void main(String[] args) throws IOException, JSONException 
 	{
